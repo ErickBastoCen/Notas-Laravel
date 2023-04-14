5c429b710bc857572767bd3c3cf95b054cc25d72
ErickBasto <erickemmanuelbastocen@gmail.com>
2023-04-13 23:14:48 -0600

    Creación de vista index - Asignaturas

---
+++ resources/views/subject/index.blade.php
@@ -0,0 +1,14 @@
+@extends ('layout/template')
+
+@section('title', 'Asignaturas | TEC MOTUL')
+
+@section('contenido')
+
+<main>
+    <div >
+        <h1 class="text-3xl font-bold underline">
+            Listado de Asignaturas
+        </h1>
+    </div>
+    <br>
+</main>
